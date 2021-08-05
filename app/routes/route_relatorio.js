4eab47c3203995b6f9875fc5a0123572d012fc5c
johnatan sousa paixao <johnatan.sousa@gmail.com>
2021-08-05 11:21:23 -0300

    commit mysql

--- app/routes/route_relatorio.js
+++ app/routes/route_relatorio.js
@@ -1,5 +1,19 @@
 module.exports = (app) => {
+
     app.get('/relatorio', (req, res) => {
-        res.render("./relatorio/relatorio")
-    })
+        const mysql = require('mysql');
+
+        let connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'controle_c_c'
+        });
+        connection.query("select * from controle;", function(error, result) {
+            //res.render("./relatorio/relatorio", { controle: result });
+            res.send(result);
+        });
+
+
+    });
 }
